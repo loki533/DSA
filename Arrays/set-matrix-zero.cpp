8b7cc2f65c42fc39c4fc49a68cd8ab706605e9cd
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-06-16 21:11:56 +0530

    added optimal approach for setZeroes

--- Arrays/set-matrix-zero.cpp
+++ Arrays/set-matrix-zero.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*Brute force Approach 
+ however this doesnt work if the marker being used pre-exists in the arrayg*/
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -38,3 +41,56 @@ public:
         }    
 }
 };
+
+/* Better approach to mark which rows and columns are supposed to b marked as zero
+-> use 2 different arrays row and column which marks the the rows and columns that are to b marked with 0
+-> Later check if either the column or row was marked & mark the correspondin element as 0*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n_rows = matrix.size();
+        int n_columns = matrix[0].size();
+
+        vector<int> rows(n_rows,0);
+        vector<int> columns(n_columns,0);
+
+        for (int i=0;i<n_rows;i++){
+            for (int j=0 ; j< n_columns;j++){
+                if (matrix[i][j] == 0){
+                    rows[i]=1;
+                    columns[j]=1;
+                }
+            }
+        }
+
+        for (int i=0;i<n_rows;i++){
+            for (int j=0;j< n_columns;j++){
+                if (rows[i] == 1 || columns[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+/*most Optimal approach
+-> use the 1st row and 1st column itself to mark if the respective rows nd columns are to b marked 0
+-> take 2 flags to track if there is a 0 in the first_row or in the first_column
+-> iterate thru rest of the matrix nd mark the row/column in the first 
+-> place 0s for the rest of the matrix
+-> place 0s for 1st row nd column if flag true*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n_columns = matrix[0].size();
+        int n_rows = matrix.size();
+
+        bool rowZero = false ;
+        bool columnZero = false ;
+
+        
+    }
+}
+
