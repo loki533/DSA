9c7f23a3a1a51d894e2a9575a9e6943be7ab6599
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-06-22 19:39:21 +0530

    added main for test

--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,40 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int columns = matrix[0].size();
+/*Approach 1
+    Check if the given target falls under the specific row
+    Condition to check 
+        if (nums[row][0] < target && nums[row][no_of_columns])
+            -> then apply binary search there*/
+class Solution{
+    public:
+        bool BinarySearch(vector<int>& nums ,int target){
 
-        for (int i=0 ; i< rows ; i++){
-            for (int j=0; j<columns ;j++){
+            int low = 0;
+            int high = nums.size()-1;
+            int mid =0;
 
-                if (matrix[i][j]==0){ /*if the present element is 0*/
+            while (low<=high){
+                mid = (low+high)/2;
 
-                    for (int k = 0 ; k < columns ; k++){ /*marks every element in the specific column 0 as -1 except the pre-exisiting 0*/
-                        if (matrix[i][k] != 0)
-                            matrix[i][k] = -1;
-                    }
-
-                    for (int l=0; l < rows ; l++){   /*marks every element in the specific row 0 as -1 except the pre-exisiting 0*/
-                        if (matrix[l][j] != 0){
-                            matrix[l][j] = -1;
-                        }
-                    }
-                    
+                if(nums[mid] > target){
+                    high = mid-1;
+                }
+                else if (nums[mid] < target){
+                    low = mid+1;
                 }
 
+                else
+                    return true;
+                
             }
+
+            return false;
         }
 
-        /* replace all the -1s with 0s*/
-        for (int i=0 ; i< rows ; i++){
-            for (int j=0; j<columns ;j++){
-                if (matrix[i][j] == -1)
-                    matrix[i][j] = 0;
+        bool SearchMatrix(vector<vector<int>>& matrix, int target){
+            int row = matrix.size();
+            int columns = matrix[0].size();
 
+            for (int i =0 ; i<row ; i++){
+                if (matrix[i][0] <= target && matrix[i][columns-1] >= target){
+                    return BinarySearch(matrix[i],target);
+                }
             }
-        }    
-}
+
+            return false;
+
+        }
+
+
+
 };
+
+int main() {
+    Solution s;
+
+    vector<vector<int>> matrix = {
+        {1,3,5,7},
+        {10,11,16,20},
+        {23,30,34,60}
+    };
+
+    cout << s.SearchMatrix(matrix, 16);
+
+    return 0;
+}
