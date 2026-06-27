04c63086cf0775960e19629115500c44a243280a
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-06-27 13:32:14 +0530

    minor fixes on middle of ll

--- Linked-list/middle-of-SLL.cpp
+++ Linked-list/middle-of-SLL.cpp
@@ -4,3 +4,33 @@ using namespace std;
 /*slow pointer -> moves one node at a time
 fast pointer -> moves 2 nodes at a atime*/
 
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data1,Node* next1){
+            data = data1;
+            next = next1;
+        }
+
+        Node(int data1){
+            data =data1;
+            next = nullptr;
+        }
+
+        Node* middleNode(Node* head){
+            Node* slow = head;
+            Node* fast = head;
+
+            while(fast != NULL && fast->next != NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+
+            return slow;
+
+        }
+
+};
+
