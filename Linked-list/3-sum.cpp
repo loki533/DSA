b5cfc02acc22062ed69b7f77179111d5351f38e0
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-07-02 21:17:45 +0530

    added optimal approach for 3-sum{

--- Linked-list/3-sum.cpp
+++ Linked-list/3-sum.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-using namespace std
+using namespace std;
 
 /*Brute force approach
     use a set to store unique vectors
@@ -28,7 +28,45 @@ using namespace std
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+
+        int n = nums.size();
+
+        for(int i=0 ;i<n;i++){
+
+            if (i>0 && nums[i] == nums[i+1])
+                continue;
+            
+            int left= i+1;
+            int right = n-1;
+
+            while(left<right){
+
+                if(nums[left]+nums[right]+nums[i] <0)
+                    left++;
+
+                else if(nums[left]+nums[right]+nums[i]>0)
+                    right--;
+                
+                else{
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    //here we are goin to check for every combination for the current ith element
+                    left++;
+                    right--;
+
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(left<right && nums[right]==nums[right+1])right--;
+
+                }
+                    
+
+            }
+
+            
+        }
+
+        return result;
         
     }
 };
