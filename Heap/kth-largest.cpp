2a859f9585cab773542d469520abd3271df5b107
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-08-08 21:12:50 +0530

    Kth largest done usin min-heap

--- Heap/kth-largest.cpp
+++ Heap/kth-largest.cpp
@@ -1,1 +1,26 @@
-inc
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        int n = nums.size();
+        if(n<k){
+            return -1;
+        }
+
+        for(int x:nums){
+            minHeap.push(x);
+        }
+
+        while(minHeap.size()>k){
+            minHeap.pop();
+        }
+
+        return minHeap.top();
+
+    }
+};
