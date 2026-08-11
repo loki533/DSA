1eb3da0dac6ba2a1f4753424c35e793be0210732
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-08-12 01:15:22 +0530

    Stack implementation using array

---
+++ Stack & Queue/stack implementation using array.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ArrayStack{
+    private:
+        int* stackarray;
+        int capacity;
+        int top;
+    public:
+
+        ArrayStack(int size = 1000){
+            capacity = size;
+            stackarray = new int[capacity];
+            top = -1;
+        }
+
+        ~ArrayStack(){
+            delete[] stackarray;
+        }
+
+        void push(int x){
+            if (top == capacity - 1){
+                cout << "Stack Overflow" << endl;
+                return ;
+            }
+            stackarray[top++] =x;
+        }
+
+        int pop(){
+            if (top == -1){
+                cout<<"Stack Underflow"<<endl;
+                return;
+            }
+            return stackarray[top--];
+        }
+
+        int top(){
+            if (top == -1){
+                cout<<"Stack Underflow"<<end;
+                return;
+            }
+            return stackarray[top];
+        }
+
+        bool isempty(){
+            return (top == -1);
+        }
+}
