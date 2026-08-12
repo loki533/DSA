c51cf0fd1c2cb902d0d3b6857cf0e0db0107b772
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-08-12 17:28:37 +0530

    Implementation of stack using queue

---
+++ Stack & Queue/stack-using-queue.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        int size = q.size();
+        q.push(x);
+
+        for(int i =0;i<size ;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int front = q.front();
+        q.pop();
+        return front;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
